e142884f606269282a2253ec6e4e7396d2da1039
SaharMach <saharmach28@gmail.com>
2024-05-02 15:59:20 +0300

    F/Footer

--- spotify-frontend/src/cmps/Footer.tsx
+++ spotify-frontend/src/cmps/Footer.tsx
@@ -1,5 +1,11 @@
 export function Footer() {
     return <div className="footer">
-        Footer
+        <section>
+            <span>
+                Preview of Spotify
+            </span>
+            <p>Sign up to get unlimited songs and podcasts with occasional ads. No credit card needed</p>
+        </section>
+        <button className="clean-btn signup-btn">Sign up for free</button>
     </div>
 }
--- spotify-frontend/src/cmps/Playlists.tsx
+++ spotify-frontend/src/cmps/Playlists.tsx
@@ -22,7 +22,7 @@ interface SearchResponse {
 
 export function Playlists() {
     const [playlists, setPlaylists] = useState<PlaylistItem[]>([]);
-
+    const play = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
     useEffect(() => {
         const fetchPlaylists = async () => {
             try {
@@ -32,7 +32,7 @@ export function Playlists() {
                         maxResults: 10,
                         q: 'top playlists in Israel', 
                         type: 'playlist', 
-                        key: '' //add api key here 
+                        key: '' 
                     }
                 });
                 setPlaylists(response.data.items);
@@ -47,11 +47,17 @@ export function Playlists() {
     
     return (
         <div className='playlist-con'>
+            
             <h1>Spotify playlist</h1>
             <section className='playlist-cards'>
 
             {playlists.map((playlist, index) => (
                 <div className='playlist-card' key={index}>
+                    <div className='card-layout'>
+                        <span>
+                            {play}
+                        </span>
+                    </div>
                     <img src={playlist.snippet.thumbnails.medium.url} alt={playlist.snippet.title} />
                     <section className='playlist-desc'>
 
