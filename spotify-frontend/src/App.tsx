309bcdbfc6b4a49f3da2be18cd539828b47e9baa
SaharMach <saharmach28@gmail.com>
2024-05-03 12:30:44 +0300

    F/LoginPage - UI

--- spotify-frontend/src/App.tsx
+++ spotify-frontend/src/App.tsx
@@ -2,6 +2,7 @@
 
 import { Home } from './pages/Home'
 import { Routes, Route } from 'react-router-dom'
+import { LoginPage } from './pages/LoginPage'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
       <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path='/login' element={<LoginPage/>}/>
       </Routes>
   )
 }
